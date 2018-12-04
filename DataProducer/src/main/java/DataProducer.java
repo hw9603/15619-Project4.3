7dcecbc6cf8bac1450b65022783e226c89a88fcf
Wen He <wenhe@andrew.cmu.edu>
2018-12-04 04:26:55 +0000

    initial commit

---
+++ DataProducer/src/main/java/DataProducer.java
@@ -0,0 +1,21 @@
+public class DataProducer {
+
+    public static void main(String[] args) {
+        /*
+            Task 1:
+            In Task 1, you need to read the content in the tracefile we give to you, 
+            and create two streams, feed the messages in the tracefile to different 
+            streams based on the value of "type" field in the JSON string.
+
+            Please note that you're working on an ec2 instance, but the streams should
+            be sent to your samza cluster. Make sure you can consume the topics on the
+            master node of your samza cluster before make a submission. 
+
+            Reference doc: https://kafka.apache.org/0100/javadoc/index.html?org/apache/kafka/clients/producer/KafkaProducer.html
+
+        */
+        //TODO: Set Properties
+
+        //TODO: Read json file and send to stream
+    }
+}
